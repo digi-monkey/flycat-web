110d3124ca9126040abce537f766f2c47be0b381
digi-monkey <digimonkey@protonmail.com>
2023-01-11 21:24:54 +0800

    add more default relays

--- src/service/relay.ts
+++ src/service/relay.ts
@@ -3,4 +3,10 @@ export const defaultRelays = [
   'wss://relay.nostr.bg/',
   'wss://nostr-relay.digitalmob.ro/',
   'wss://jiggytom.ddns.net/',
+  'wss://relay.damus.io/',
+  'wss://relay.stoner.com/',
+  'wss://nostr-pub.wellorder.net/',
+  'wss://nostr.bitcoiner.social/',
+  'wss://relay.taxi/',
+  'wss://nostr-verified.wellorder.net/',
 ];
