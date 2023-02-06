3accfa0a4eb1dd920eddd0fefadd21b3f987b6fc
digi-monkey <digimonkey@protonmail.com>
2023-02-07 06:23:31 +0800

    add tips article

--- src/app/pages/Backup/Backup.tsx
+++ src/app/pages/Backup/Backup.tsx
@@ -153,7 +153,10 @@ export function Backup({ isLoggedIn, myPublicKey, myCustomRelay }) {
         {isLocalRelayTips && (
           <div style={{ marginBottom: '20px' }}>
             {t('backup.tips')}
-            <a href="/article/" target={'_blank'}>
+            <a
+              href="/article/45c41f21e1cf715fa6d9ca20b8e002a574db7bb49e96ee89834c66dac5446b7a/47858"
+              target={'_blank'}
+            >
               {t('backup.thisPost')}
             </a>
           </div>
