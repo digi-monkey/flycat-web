46f43d6f9fc800d7efdfa5a0724d64f4f4d7f031
digi-monkey <digimonkey@protonmail.com>
2023-06-10 10:36:36 +0800

    wip: relay last update time

---
+++ src/service/relay/util.ts
@@ -0,0 +1,12 @@
+import { Relay } from "./type";
+
+export function isRelayOutdate(relay: Relay){
+	const threshold = 2 * 24 * 60 * 60 * 1000; // 2 days in milliseconds
+  const currentTime = new Date().getTime();
+	if(relay.lastUpdateTimestamp){
+		const difference = relay.lastUpdateTimestamp - currentTime;
+		return difference > threshold;
+	}
+
+	return true;
+}
