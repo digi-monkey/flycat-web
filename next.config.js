f2145a6b3fded2d1c40dd94d274cdb5bb72ba6b1
ShookLyngs <shooklyngs@gmail.com>
2023-07-12 17:41:47 +0800

    fix: remove skipWaiting

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { i18n } = require('./next-i18next.config.js');
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 })
 
