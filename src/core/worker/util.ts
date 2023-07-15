a2a8a5beeaf381aa6ed39520fc95ae222f924891
digi-monkey <105776364+digi-monkey@users.noreply.github.com>
2023-07-15 12:50:18 +0800

    feat: add subscribe community (#180)

--- src/core/worker/util.ts
+++ src/core/worker/util.ts
@@ -1,4 +1,9 @@
+import { Event } from 'core/nostr/Event';
 import { CallRelay, CallRelayType } from './type';
+import { CallWorker } from './caller';
+import { EventTags, PublicKey, WellKnownEventKind } from 'core/nostr/type';
+import { RawEvent } from 'core/nostr/RawEvent';
+import { Dispatch, SetStateAction } from 'react';
 
 export function createCallRelay(newConn: string[]): CallRelay {
   const type =
@@ -7,4 +12,196 @@ export function createCallRelay(newConn: string[]): CallRelay {
     type,
     data: newConn,
   };
+}
+
+export async function getContactEvent({
+  worker,
+  pk,
+}: {
+  worker: CallWorker;
+  pk: PublicKey;
+}) {
+  let event: Event | null = null;
+  const dataStream = worker.subContactList([pk]).getIterator();
+  for await (const data of dataStream) {
+    if (data.event.kind !== WellKnownEventKind.contact_list) continue;
+    if (data.event.pubkey !== pk) continue;
+
+    if (!event) {
+      event = data.event;
+      continue;
+    }
+
+    if (event.created_at < data.event.created_at) {
+      event = data.event;
+      continue;
+    }
+  }
+  return event;
+}
+
+export async function updateMyContactEvent({
+  worker,
+  pk,
+  setMyContactEvent,
+}: {
+  worker: CallWorker;
+  pk: PublicKey;
+  setMyContactEvent: Dispatch<SetStateAction<Event | undefined>>;
+}) {
+  const event = await getContactEvent({ worker: worker!, pk });
+  if (!event) return;
+
+  setMyContactEvent(prev => {
+    if (!prev) return event;
+
+    if (prev.created_at < event.created_at) return event;
+
+    return prev;
+  });
+}
+
+export function isFollowed(
+  contactEvent: Event,
+  target: {
+    type: 'people' | 'hashTag' | 'community';
+    data: string;
+  },
+): boolean {
+  if (target.type === 'people') {
+    return (
+      contactEvent.tags.filter(
+        t => t[0] === EventTags.P && t[1] === target.data,
+      ).length > 0
+    );
+  }
+  if (target.type === 'hashTag') {
+    return (
+      contactEvent.tags.filter(
+        t => t[0] === EventTags.T && t[1] === target.data,
+      ).length > 0
+    );
+  }
+  if (target.type === 'community') {
+    return (
+      contactEvent.tags.filter(
+        t => t[0] === EventTags.A && t[1] === target.data,
+      ).length > 0
+    );
+  }
+
+  throw new Error('unknown target type');
+}
+
+export function createFollowContactEvent(
+  contactEvent: Event,
+  target: {
+    type: 'people' | 'hashTag' | 'community';
+    data: string;
+  },
+) {
+  const isFollow = isFollowed(contactEvent, target);
+  if (isFollow) {
+    throw new Error('already followed!');
+  }
+
+  const newEvent: RawEvent = new RawEvent(
+    '',
+    contactEvent.kind,
+    contactEvent.tags,
+    contactEvent.content,
+  );
+
+  if (target.type === 'people') {
+    newEvent.tags.push([EventTags.P, target.data, '']);
+    return newEvent;
+  }
+
+  if (target.type === 'hashTag') {
+    newEvent.tags.push([EventTags.T, target.data]);
+    return newEvent;
+  }
+
+  if (target.type === 'community') {
+    newEvent.tags.push([EventTags.A, target.data, '']);
+    return newEvent;
+  }
+
+  throw new Error('unknown target type');
+}
+
+export function createInitialFollowContactEvent(
+  target: {
+    type: 'people' | 'hashTag' | 'community';
+    data: string;
+  },
+) {
+  const newEvent: RawEvent = new RawEvent(
+    '',
+    WellKnownEventKind.contact_list,
+    undefined,
+  );
+
+  if (target.type === 'people') {
+    newEvent.tags.push([EventTags.P, target.data, '']);
+    return newEvent;
+  }
+
+  if (target.type === 'hashTag') {
+    newEvent.tags.push([EventTags.T, target.data]);
+    return newEvent;
+  }
+
+  if (target.type === 'community') {
+    newEvent.tags.push([EventTags.A, target.data, '']);
+    return newEvent;
+  }
+
+  throw new Error('unknown target type');
+}
+
+export function createUnFollowContactEvent(
+  contactEvent: Event,
+  target: {
+    type: 'people' | 'hashTag' | 'community';
+    data: string;
+  },
+) {
+  const isFollow = isFollowed(contactEvent, target);
+  if (!isFollow) {
+    throw new Error('need follow first!');
+  }
+
+  const newEvent: RawEvent = new RawEvent(
+    '',
+    contactEvent.kind,
+    contactEvent.tags,
+    contactEvent.content,
+  );
+
+  if (target.type === 'people') {
+    const tags = newEvent.tags.filter(
+      t => (t[0]!== EventTags.P) ||  (t[0] === EventTags.P && t[1] !== target.data),
+    );
+    newEvent.tags = tags;
+    return newEvent;
+  }
+
+  if (target.type === 'hashTag') {
+    const tags = newEvent.tags.filter(
+      t => (t[0]!== EventTags.T) || (t[0] === EventTags.T && t[1] !== target.data),
+    );
+    newEvent.tags = tags;
+    return newEvent;
+  }
+
+  if (target.type === 'community') {
+    const tags = newEvent.tags.filter(
+      t => (t[0]!== EventTags.A) || (t[0] === EventTags.A && t[1] !== target.data),
+    );
+    newEvent.tags = tags;
+    return newEvent;
+  }
+
+  throw new Error('unknown target type');
 }
--- src/pages/explore/index.page.tsx
+++ src/pages/explore/index.page.tsx
@@ -9,33 +9,43 @@ import { CommunityMetadata, Nip172 } from 'core/nip/172';
 import {
   EventMap,
   EventSetMetadataContent,
+  EventTags,
+  Filter,
   Naddr,
   UserMap,
   WellKnownEventKind,
 } from 'core/nostr/type';
-import { Button, Divider, Input, Space, Tabs } from 'antd';
-import { Community } from './community/community';
+import { Avatar, Input, List, Tabs } from 'antd';
 import { deserializeMetadata } from 'core/nostr/content';
 import { useLoadProfiles } from './hooks/useLoadProfile';
 
 import PageTitle from 'components/PageTitle';
 import styles from './index.module.scss';
-import { SearchOutlined } from '@ant-design/icons';
-
-const { Search } = Input;
+import Icon from 'components/Icon';
+import { EventWithSeen } from 'pages/type';
+import PostItems from 'components/PostItems';
+import { useMyPublicKey } from 'hooks/useMyPublicKey';
+import { updateMyContactEvent } from 'core/worker/util';
+import { setEventWithSeenMsgList } from 'pages/helper';
+import Link from 'next/link';
 
 const Explore = () => {
   const { t } = useTranslation();
-
+  const myPublicKey = useMyPublicKey();
   const { worker, newConn } = useCallWorker();
 
+  const [myContactEvent, setMyContactEvent] = useState<Event>();
   const [userMap, setUserMap] = useState<UserMap>(new Map());
   const [eventMap, setEventMap] = useState<EventMap>(new Map());
   const [communities, setCommunities] = useState<Map<Naddr, CommunityMetadata>>(
     new Map(),
   );
-  const [selectedCommunityId, setSelectedCommunityId] = useState<Naddr>();
+  const [msgList, setMsgList] = useState<EventWithSeen[]>([]);
+  const [followCommMsgList, setFollowCommMsgList] = useState<EventWithSeen[]>(
+    [],
+  );
   const [searchName, setSearchName] = useState<string>();
+  const [selectTabKey, setSelectTabKey] = useState<string>('All Tribes');
 
   const handleEvent = (event: Event, relayUrl?: string) => {
     switch (event.kind) {
@@ -74,81 +84,214 @@ const Explore = () => {
         });
         break;
 
+      case Nip172.approval_kind:
+        {
+          const approvedEvent = Nip172.parseNoteFromApproval(event);
+          if (approvedEvent) {
+            setMsgList(oldArray => {
+              if (!oldArray.map(e => e.id).includes(approvedEvent.id)) {
+                // do not add duplicated msg
+
+                // save event
+                const newItems = [
+                  ...oldArray,
+                  { ...approvedEvent, ...{ seen: [relayUrl!] } },
+                ];
+                // sort by timestamp
+                const sortedItems = newItems.sort((a, b) =>
+                  a.created_at >= b.created_at ? -1 : 1,
+                );
+                return sortedItems;
+              } else {
+                const id = oldArray.findIndex(s => s.id === approvedEvent.id);
+                if (id === -1) return oldArray;
+
+                if (!oldArray[id].seen?.includes(relayUrl!)) {
+                  oldArray[id].seen?.push(relayUrl!);
+                }
+              }
+              return oldArray;
+            });
+          }
+        }
+
+        break;
+
       default:
         break;
     }
   };
 
   useEffect(() => {
-    if (communities.size > 0 && !selectedCommunityId) {
-      setSelectedCommunityId(Array.from(communities.keys())[0]);
+    if (communities.size > 0 && worker) {
+      const addrs = Array.from(communities.keys());
+      const filter: Filter = {
+        kinds: [Nip172.approval_kind],
+        '#a': addrs,
+        limit: 50,
+      };
+      worker
+        .subFilter({
+          filter,
+        })
+        .iterating({ cb: handleEvent });
     }
-  }, [communities]);
+  }, [communities, worker]);
+
+  useEffect(() => {
+    if (!worker) return;
+    if (myPublicKey.length === 0) return;
+
+    updateMyContactEvent({worker, pk: myPublicKey, setMyContactEvent});
+  }, [worker, myPublicKey, newConn]);
+
+  useEffect(() => {
+    if (
+      selectTabKey === 'Following' &&
+      myPublicKey.length > 0 &&
+      worker &&
+      myContactEvent
+    ) {
+      const addrs = myContactEvent.tags
+        .filter(
+          t =>
+            t[0] === EventTags.A &&
+            (t[1] as string).startsWith(
+              `${WellKnownEventKind.community_metadata}:`,
+            ),
+        )
+        .map(t => t[1] as Naddr);
+      const filter: Filter = {
+        kinds: [Nip172.approval_kind],
+        '#a': addrs,
+        limit: 50,
+      };
+      worker
+        .subFilter({
+          filter,
+        })
+        .iterating({
+          cb: (event, relayUrl) => {
+            if (event.kind !== WellKnownEventKind.community_approval) return;
+            const targetEvent = Nip172.parseNoteFromApproval(event);
+            if (targetEvent)
+              setEventWithSeenMsgList(
+                targetEvent,
+                relayUrl!,
+                setFollowCommMsgList,
+              );
+          },
+        });
+    }
+  }, [myContactEvent, myPublicKey, worker, selectTabKey]);
 
   useLoadCommunities({ worker, newConn, handleEvent });
   useLoadProfiles({ worker, handleEvent, newConn, communities });
+
+  const tabsItems = ['All Tribes', 'Following'].map(name => {
+    return {
+      key: name,
+      label: name,
+    };
+  });
+
+  const renderContent = () => {
+    if (selectTabKey === 'All Tribes') {
+      return (
+        <PostItems
+          msgList={msgList.slice(0, 50)}
+          worker={worker!}
+          userMap={userMap}
+          eventMap={eventMap}
+          relays={worker?.relays.map(r => r.url) || []}
+        />
+      );
+    }
+
+    if (selectTabKey === 'Following') {
+      return (
+        <PostItems
+          msgList={followCommMsgList.slice(0, 50)}
+          worker={worker!}
+          userMap={userMap}
+          eventMap={eventMap}
+          relays={worker?.relays.map(r => r.url) || []}
+        />
+      );
+    }
+  };
+
+  const commCardListData = Array.from(communities.keys())
+    .filter(k =>
+      searchName ? k.toLowerCase().includes(searchName.toLowerCase()) : true,
+    )
+    .map(k => communities.get(k))
+    .filter(v => v != null) as CommunityMetadata[];
+
   return (
     <BaseLayout>
       <Left>
         <PageTitle title={'Explore'} />
+        <div className={styles.explorePanel}>
+          <div className={styles.searchGroup}>
+            <Input
+              placeholder="community name"
+              prefix={<Icon type="icon-search" />}
+              onChange={e => setSearchName(e.target.value)}
+            />
+            <Icon onClick={()=>window.open("/explore/community/list")} className={styles.commList} type="icon-rule-mode" />
+          </div>
 
-        <div className={styles.communitySearchPanel}>
-          <Input
-            size="large"
-            addonBefore={<SearchOutlined />}
-            placeholder="Search community name"
-            onChange={e => setSearchName(e.target.value)}
-          />
+          <div className={styles.posts}>
+            <List
+              pagination={{
+                responsive: true,
+                simple: true,
+                defaultCurrent: 10,
+                total: commCardListData.length,
+              }}
+              grid={{
+                gutter: 16,
+                xs: 5,
+                sm: 5,
+                md: 5,
+                lg: 5,
+                xl: 5,
+                xxl: 5,
+              }}
+              dataSource={commCardListData}
+              renderItem={item => (
+                <List.Item>
+                  <div
+                    className={styles.commCardListItem}
+                    onClick={() =>
+                      window.open(
+                        `/explore/community/` +
+                          Nip172.communityAddr({
+                            identifier: item.id,
+                            author: item.creator,
+                          }),
+                      )
+                    }
+                  >
+                    <Avatar size={'small'} src={item.image} />
+                    <div className={styles.name}>
+                      {item.id.length > 0 ? item.id : 'unnamed'}
+                    </div>
+                  </div>
+                </List.Item>
+              )}
+            />
 
-          <Button
-            type="primary"
-            size="large"
-            onClick={() => window.open('/explore/community/create')}
-          >
-            create new one
-          </Button>
+            <Tabs
+              items={tabsItems}
+              defaultValue={selectTabKey}
+              onChange={val => setSelectTabKey(val)}
+            />
+          </div>
         </div>
 
-        <Divider orientation="left">All Communities</Divider>
-        <div className={styles.communityTab}>
-          <Tabs
-            tabPosition="top"
-            activeKey={selectedCommunityId}
-            onChange={naddr => {
-              setSelectedCommunityId(naddr);
-            }}
-            items={
-              searchName
-                ? Array.from(communities.keys())
-                    .filter(a => a.includes(searchName))
-                    .map(naddr => {
-                      const community = communities.get(naddr)!;
-                      return {
-                        label: community.id,
-                        key: naddr,
-                      };
-                    })
-                : Array.from(communities.keys()).map(naddr => {
-                    const community = communities.get(naddr)!;
-                    return {
-                      label: community.id,
-                      key: naddr,
-                    };
-                  })
-            }
-          />
-        </div>
-
-        {selectedCommunityId && (
-          <Community
-            worker={worker}
-            setEventMap={setEventMap}
-            setUserMap={setUserMap}
-            userMap={userMap}
-            eventMap={eventMap}
-            community={communities.get(selectedCommunityId)!}
-          />
-        )}
+        {renderContent()}
       </Left>
       <Right></Right>
     </BaseLayout>
