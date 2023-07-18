c6403047ca8c83b8aee9e4fff4af8cbb55b2ab1d
digi-monkey <digimonkey@protonmail.com>
2023-07-18 14:39:58 +0800

    fix: display connected relay in selector

--- src/components/RelaySelector/util.ts
+++ src/components/RelaySelector/util.ts
@@ -1,5 +1,6 @@
 import { RelayGroupMap } from 'core/relay/group/type';
 import { RelayMode, RelayModeSelectMenus, RelayModeSelectOption, toLabel } from './type';
+import { WsConnectStatus } from 'core/worker/type';
 
 export function getSelectGroupId(groups: RelayGroupMap) {
   return Array.from(groups.keys())
@@ -51,4 +52,15 @@ export function getFooterMenus() {
 export function isFastestRelayOutdated(timestamp: number, threshold: number = 5 * 60 * 1000): boolean {
   const currentTime = Date.now();
   return currentTime - timestamp > threshold;
+}
+
+export function toConnectStatus(label: string, wsConnectStatus: WsConnectStatus){
+  const all = Array.from(wsConnectStatus).length;
+  const connected = Array.from(wsConnectStatus).filter(w => w[1] === true).length;
+  return label.split("(")[0] + ` (${connected}/${all})`
+}
+
+export function getConnectedRelayUrl(wsConnectStatus: WsConnectStatus){
+  const connected = Array.from(wsConnectStatus).filter(w => w[1] === true).map(w => w[0]);
+  return connected;
 }
