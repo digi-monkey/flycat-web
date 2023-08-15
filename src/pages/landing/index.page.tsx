3836d85837fb0e32e3057f2b9ccd9cd0f66caf0f
digi-monkey <105776364+digi-monkey@users.noreply.github.com>
2023-08-15 18:40:03 +0800

    feat: add landing page for pwa install (#217)
    * feat: add landing page for pwa install
    
    * chore: add install tips in homepage
    
    * chore: detact brwoser environment
    
    * chore: add message error
    
    * chore: refactor unsupported message
    
    * chore: wording

---
+++ src/pages/landing/index.page.tsx
@@ -0,0 +1,80 @@
+import { Button, Col, Divider, Row } from 'antd';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { DeviceFrameset } from 'react-device-frameset';
+
+import styles from './index.module.scss';
+import InstallButton from './install';
+import 'react-device-frameset/styles/marvel-devices.min.css';
+
+export const Landing: React.FC = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <img src="logo512.png" alt="" />
+      </div>
+      <Row>
+        <Col xs={24} sm={12} md={12} lg={12}>
+          <div className={styles.instr}>
+            <div className={styles.title}>
+              Exit <span>Toxic</span> Internet
+            </div>
+            <div className={styles.description}>
+              <p>
+                Flycat is a{' '}
+                <a href="https://github.com/digi-monkey/flycat-web">
+                  open source
+                </a>{' '}
+                social network built on{' '}
+                <a href="https://nostr.how/en/what-is-nostr">Nostr protocol</a>.
+              </p>
+              <p>
+                There is NO follower count, like button, private recommendation
+                algorithm and all the other bullshit components found in modern
+                social networks.{' '}
+              </p>
+              <p>
+                Your identity is just a crypto key pair controlled by yourself.
+                Like a Bitcoin address.
+              </p>
+              <p>
+                You can choose which server to connect to and migrate your data
+                as you go. You can even run your own server which is only
+                accessible by your friends and family.
+              </p>
+            </div>
+            <Divider></Divider>
+            <div className={styles.action}>
+              <InstallButton />
+              <Button
+                type="link"
+                onClick={() =>
+                  window.open('https://github.com/digi-monkey/flycat-web')
+                }
+              >
+                Learn More
+              </Button>
+              <div className={styles.manualInstall}>
+                If the above button won&apos;t work, just tap the share button in
+                your browser and select Add to Home Screen on iOS or Install
+                on Android.
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col xs={0} sm={12} md={12} lg={12}>
+          <DeviceFrameset device="iPhone 8" color="silver">
+            <img className={styles.img} src="images/landing.png" alt="" />
+          </DeviceFrameset>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Landing;
+
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
---
+++ src/pages/landing/install.tsx
@@ -0,0 +1,62 @@
+import { Button, message } from 'antd';
+import { useState, useEffect } from 'react';
+
+const InstallButton: React.FC = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: any) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    };
+
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+      window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    }
+
+    return () => {
+      window.removeEventListener(
+        'beforeinstallprompt',
+        handleBeforeInstallPrompt,
+      );
+    };
+  }, []);
+
+  const handleInstallClick = async () => {
+    if (!('serviceWorker' in navigator && 'PushManager' in window)) {
+      return messageApi.warning(
+        'Device Not supported. Please manually Tap the share button in your browser and select "Add to Home Screen" on iOS or "Install" on Android.',
+      );
+    }
+
+    if (deferredPrompt) {
+      try {
+        await deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice;
+
+        if (choiceResult.outcome === 'accepted') {
+          messageApi.success('PWA installation accepted');
+        } else {
+          messageApi.error('PWA installation dismissed');
+        }
+        setDeferredPrompt(null);
+      } catch (error: any) {
+        messageApi.error(error.message);
+      }
+    } else {
+      return messageApi.error('deferredPrompt is null');
+    }
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Button size="large" type="primary" onClick={handleInstallClick}>
+        Install PWA App
+      </Button>
+    </>
+  );
+};
+
+export default InstallButton;
