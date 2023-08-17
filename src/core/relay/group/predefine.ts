01a6edd08c61c326660609a3cab80b46f05da2d9
digi-monkey <digimonkey@protonmail.com>
2023-08-17 23:33:14 +0800

    wip: add predifine gorups

---
+++ src/core/relay/group/predefine.ts
@@ -0,0 +1,13 @@
+export const preDefineRelayGroups = [
+  {
+    id: 'Japan-general',
+    urls: [
+      'wss://relay-jp.nostr.wirednet.jp',
+      'wss://nostr-paid.h3z.jp',
+      'wss://nostr.holybea.com',
+      'wss://nostr.fediverse.jp',
+    ],
+  },
+  { id: 'nostr-dev', urls: ['wss://relay.stoner.com'] },
+  { id: 'christ-only', urls: ['wss://christpill.nostr1.com'] },
+];
