9801f399ee34de6590ccf77ec7173cef2a6257f5
digi-monkey <105776364+digi-monkey@users.noreply.github.com>
2023-09-11 23:47:16 +0800

    chore: bump joyId to v0.1.0 and use stable testnet url (#269)

---
+++ src/core/joyid/config.ts
@@ -0,0 +1,8 @@
+export const joyIdConfig = {
+	// your app name
+	name: 'flycat',
+	// your app logo,
+	logo: 'https://flycat.club/logo512.png',
+	// optional, default to 'https://poc.joyid.dev'
+	joyidAppURL: 'https://testnet.joyid.dev',
+}
--- src/pages/login/LoginCard.tsx
+++ src/pages/login/LoginCard.tsx
@@ -215,7 +215,7 @@ const LoginCard = ({ isLoggedIn, doLogin }: LoginFormProps) => {
 
         <Button className={styles.button} onClick={signWithJoyId}>
           <img className={styles.icon} style={{transform: "scale(2)"}} src="./icon/joyid-logo.svg" />
-          {'JoyID'}
+          {'JoyID (testnet)'}
         </Button>
 
         <Button className={styles.button} onClick={signWithEthWallet}>
