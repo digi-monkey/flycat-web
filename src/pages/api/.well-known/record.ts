bcb021871fa0038668bf8516325f6ea8b04ce749
digi-monkey <digimonkey@protonmail.com>
2023-09-28 09:50:30 +0800

    fix(nip-05): assgin monkey id to the author

--- src/pages/api/.well-known/record.ts
+++ src/pages/api/.well-known/record.ts
@@ -1,3 +1,3 @@
 export const records: Record<string, string> = {
-	electronicMonkey: '45c41f21e1cf715fa6d9ca20b8e002a574db7bb49e96ee89834c66dac5446b7a',
+	monkey: '45c41f21e1cf715fa6d9ca20b8e002a574db7bb49e96ee89834c66dac5446b7a',
 };
