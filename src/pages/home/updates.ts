013ce528d424d75836d74ee0d084b38e0a115e6c
digi-monkey <digimonkey@protonmail.com>
2023-09-28 08:59:49 +0800

    chore(home): right page clickable

---
+++ src/pages/home/updates.ts
@@ -0,0 +1,13 @@
+export const updates = [
+  {
+    content:
+      '[9.27] v0.2.5 release: add hashtags and nip05-name/dotbit-alias in user profile/article URL',
+    isNew: true,
+    url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.5',
+  },
+  {
+    content: '[9.13]  v0.2.4 release with timeline refactoring',
+    isNew: false,
+    url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.4',
+  },
+];
