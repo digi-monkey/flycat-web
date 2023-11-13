1b6a41d0f8f6e05ad2b7cd659489b4f7bc4a74f2
digi-monkey <digimonkey@protonmail.com>
2023-11-14 00:28:42 +0800

    perf(pubNoteTextarea): query mention/comm on local db first

--- src/components/PubNoteTextarea/hooks.tsx
+++ src/components/PubNoteTextarea/hooks.tsx
@@ -1,25 +1,15 @@
-import { RootState } from 'store/configureStore';
-import { seedRelays } from "core/relay/pool/seed";
-import { useSelector } from "react-redux";
-import { useCallWorker } from "hooks/useWorker";
-import { CallRelayType } from "core/worker/type";
 import { useEffect, useState } from "react";
 import { useReadonlyMyPublicKey } from "hooks/useMyPublicKey";
 import { deserializeMetadata } from 'core/nostr/content';
 import {
-  EventContactListPTag,
-  EventSetMetadataContent,
-  EventTags,
+  Filter,
   Naddr,
-  PublicKey,
-  UserMap,
-  WellKnownEventKind
 } from 'core/nostr/type';
-import { Event } from 'core/nostr/Event';
 import styles from './index.module.scss';
 import { CallWorker } from 'core/worker/caller';
-import { createCallRelay } from 'core/worker/util';
 import { CommunityMetadata, Nip172 } from 'core/nip/172';
+import { contactQuery, dbQuery, profileQuery } from 'core/db';
+import { parsePubKeyFromTags } from 'pages/helper';
 
 export interface IMentions {
   key: string,
@@ -27,98 +17,40 @@ export interface IMentions {
   label: React.ReactNode
 }
 
-export function useLoadContacts({
-  worker,
-  newConn,
-}: {
-  worker?: CallWorker;
-  newConn: string[];
-}) {
-  const myPublicKey = useReadonlyMyPublicKey();
-  const isLoggedIn = useSelector((state: RootState) => state.loginReducer.isLoggedIn);
-
-  const [userMap, setUserMap] = useState<UserMap>(new Map());
-  const [userContactList, setUserContactList] = useState<{ keys: PublicKey[]; created_at: number }>({ keys: [], created_at: 0 });
-
-  function handleEvent(event: Event, relayUrl?: string) {
-    if (event.kind === WellKnownEventKind.set_metadata) {
-      const metadata: EventSetMetadataContent = deserializeMetadata(event.content);
-      setUserMap(prev => {
-        const newMap = new Map(prev);
-        const oldData = newMap.get(event.pubkey);
-        if (oldData && oldData.created_at > event.created_at) return newMap;
-
-        newMap.set(event.pubkey, {
-          ...metadata,
-          ...{ created_at: event.created_at },
-        });
-
-        return newMap;
-      });
-    }
-
-    if (event.kind === WellKnownEventKind.contact_list) {
-      if (event.pubkey === myPublicKey) {
-        setUserContactList(prev => {
-          if (prev && prev?.created_at >= event.created_at) return prev;
-
-          const keys = (
-            event.tags.filter(
-              t => t[0] === EventTags.P,
-            ) as EventContactListPTag[]
-          ).map(t => t[1]);
-
-          return {
-            keys,
-            created_at: event.created_at,
-          };
-        });
-      }
-    }
-  }
-
-  useEffect(() => {
-    const pks = userContactList?.keys || [];
-    if (isLoggedIn && myPublicKey.length > 0) pks.push(myPublicKey);
-    if (pks.length === 0) return;
-
-    worker
-      ?.subMetaDataAndContactList(pks, undefined, {
-        type: CallRelayType.batch,
-        data: newConn,
-      })
-      ?.iterating({ cb: handleEvent });
-  }, [newConn, myPublicKey]);
-
-  return { userMap, userContactList };
-}
-
 export function useSetMentions(
   setMentionsValue: React.Dispatch<React.SetStateAction<IMentions[]>>,
-  userMap: UserMap
 ) {
   const myPublicKey = useReadonlyMyPublicKey();
-
-  // todo: this render too many times, maybe just set value before user typing
-  useEffect(() => {
-    if (userMap.size === 0) return;
-
-    setMentionsValue([]);
-    const mentions = Array.from(userMap.entries())?.filter(u => u[0] !== myPublicKey).reduce((result, [pk, user]) => {
-      const name = user.name || user.display_name || "...";
-      result.push({
-        key: pk,
-        value: name,
-        label: <div className={styles.mentions}>
-          <img src={user?.picture} alt="picture" />
-          <span>{name}</span>
-        </div>
+  const getContactToSetMentions = async () => {
+    const contact = await contactQuery.getContactByPubkey(myPublicKey);
+    if(contact){
+      const followings = parsePubKeyFromTags(contact.tags);
+      const profiles = (await profileQuery.getBatchProfiles(followings)).map(e => {
+        return {
+          pk: e.pubkey,
+          ...deserializeMetadata(e.content)
+        }
       });
-      return result;
-    }, [] as IMentions[]);
-    if (mentions.length > 0)
+      const mentions = profiles.map(user => {
+        const name = user.name || user.display_name || "...";
+        const mention: IMentions = {
+          key: user.pk,
+          value: name,
+          label: <div className={styles.mentions}>
+            <img src={user?.picture} alt="picture" />
+            <span>{name}</span>
+          </div>
+        }
+        return mention;
+      });
       setMentionsValue(mentions);
-  }, [userMap.size]);
+    }
+
+    
+  }
+  useEffect(()=>{
+    getContactToSetMentions();
+  }, [myPublicKey]); 
 }
 
 export function useLoadCommunities({
@@ -133,10 +65,14 @@ export function useLoadCommunities({
     new Map(),
   );
 
-  const handleEvent = (event: Event, relayUrl?: string) => {
-    switch (event.kind) {
-      case Nip172.metadata_kind:
-        const metadata = Nip172.parseCommunityMetadata(event);
+  const queryCommunity = async (filter: Filter) => {
+    const events = await dbQuery.matchFilterRelay(filter, []);
+    if(events.length === 0){
+      return worker?.subFilter({ filter });
+    }
+
+    events.filter(e => e.kind === Nip172.metadata_kind).map(event => {
+      const metadata = Nip172.parseCommunityMetadata(event);
         const addr = Nip172.communityAddr({
           identifier: metadata.id,
           author: metadata.creator,
@@ -146,20 +82,15 @@ export function useLoadCommunities({
           newMap.set(addr, metadata);
           return newMap;
         });
-        break;
-
-      default:
-        break;
-    }
-  };
+    })
+  }
 
   useEffect(() => {
     if (!worker) return;
 
-    const callRelay = createCallRelay(newConn);
     const filter = Nip172.communitiesFilter();
-    worker.subFilter({ filter, callRelay }).iterating({ cb: handleEvent });
-  }, [newConn, worker]);
+    queryCommunity(filter);
+  }, [worker]);
 
   return communities;
 }
