2c2e4a711e4c40e3b6cb0f8afe45a70acc4b19fc
digi-monkey <digimonkey@protonmail.com>
2023-12-06 01:08:43 +0800

    fix: use @ant-design/cssinjs in _document.tsx

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,6 +1,7 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 
-export default class FlyCatDocument extends Document {
+class FlyCatDocument extends Document {
   render() {
     return (
       <Html>
@@ -131,3 +132,32 @@ export default class FlyCatDocument extends Document {
     );
   }
 }
+
+// ref: https://ant.design/docs/react/use-with-next-cn#%E4%BD%BF%E7%94%A8-pages-router
+
+FlyCatDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const style = extractStyle(cache, true);
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  };
+};
+
+export default FlyCatDocument;
