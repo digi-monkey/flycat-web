997449ff8ecdad8e739398b831f7d3810f49fe62
digi-monkey <digimonkey@protonmail.com>
2023-12-13 10:30:06 +0800

    wip

--- src/components/MsgFeed/hook/useSubMsg.ts
+++ src/components/MsgFeed/hook/useSubMsg.ts
@@ -13,7 +13,7 @@ export function useSubMsg({
   worker,
   latest,
 }: {
-  setNewComingMsg: Dispatch<SetStateAction<DbEvent[]>>,
+  setNewComingMsg: Dispatch<SetStateAction<DbEvent[]>>;
   msgFilter?: Filter;
   isValidEvent?: (event: Event) => boolean;
   worker: CallWorker | undefined;
@@ -27,37 +27,35 @@ export function useSubMsg({
     if (!msgFilter || !validateFilter(msgFilter)) return;
 
     let since = msgFilter.since;
-    if(latest){
-      if(since == null){
+    if (latest) {
+      if (since == null) {
         since = latest;
-      }else{
-        if(latest > since){
+      } else {
+        if (latest > since) {
           since = latest;
         }
       }
-    }else{
-      if(since == null){
+    } else {
+      if (since == null) {
         since = 0;
       }
     }
-    const filter = {...msgFilter, since};
+    const filter = { ...msgFilter, since };
 
     const pks: string[] = [];
     let events: Event[] = [];
 
-    console.debug(
-      'start sub msg..',
-      filter,
-      isValidEvent,
-      typeof isValidEvent,
-    );
-    const dataStream = worker
-      .subFilter({ filter })
-      .getIterator();
+    console.debug('start sub msg..', filter, isValidEvent, typeof isValidEvent);
+    const dataStream = worker.subFilter({ filter }).getIterator();
     for await (const data of dataStream) {
       const event = data.event;
       if (isValidEvent) {
         if (!isValidEvent(event)) {
+          continue;
+        }
+      }
+      if (latest) {
+        if (event.created_at <= latest) {
           continue;
         }
       }
@@ -67,79 +65,79 @@ export function useSubMsg({
         pks.push(event.pubkey);
       }
     }
-  
+
     events = events
-        .filter(e => {
-          if (e.kind === WellKnownEventKind.community_approval) {
-            try {
-              const targetEvent = JSON.parse(e.content);
-              if (latest && targetEvent.created_at <= latest) {
-                return false;
-              }
-            } catch (error) {
+      .filter(e => {
+        if (e.kind === WellKnownEventKind.community_approval) {
+          try {
+            const targetEvent = JSON.parse(e.content);
+            if (latest && targetEvent.created_at <= latest) {
               return false;
             }
+          } catch (error) {
+            return false;
           }
-          return true;
-        })
-        .map(e => {
-          if (e.kind === WellKnownEventKind.community_approval) {
-            const event = { ...e, ...(JSON.parse(e.content) as DbEvent) };
-            return event;
-          }
-          return e;
-        });
-      events = mergeAndSortUniqueDbEvents(events as any, events as any);
-      console.log('sub diff: ', events, events.length, filter);
-      setNewComingMsg(prev => mergeAndSortUniqueDbEvents(events as any, prev));
+        }
+        return true;
+      })
+      .map(e => {
+        if (e.kind === WellKnownEventKind.community_approval) {
+          const event = { ...e, ...(JSON.parse(e.content) as DbEvent) };
+          return event;
+        }
+        return e;
+      });
+    events = mergeAndSortUniqueDbEvents(events as any, events as any);
+    console.log('sub diff: ', events, events.length, filter);
+    setNewComingMsg(prev => mergeAndSortUniqueDbEvents(events as any, prev));
 
     dataStream.unsubscribe();
     console.debug('finished sub msg!');
 
     // sub user profiles
     if (pks.length > 0) {
-      worker
-        .subFilter({
-          filter: {
-            kinds: [WellKnownEventKind.set_metadata],
-            authors: pks,
-          },
-        })
+      worker.subFilter({
+        filter: {
+          kinds: [WellKnownEventKind.set_metadata],
+          authors: pks,
+        },
+      });
     }
   };
 
   const clearProcess = () => {
-    if(intervalId){
+    if (intervalId) {
       clearInterval(intervalId);
-      console.debug("clear interval id", intervalId);
-      setIntervalId(prev => undefined);
+      console.debug('clear interval id', intervalId);
+      setIntervalId(undefined);
     }
-  }
+  };
 
   const startProcess = (seconds: number) => {
-    if(intervalId == null){
+    if (intervalId == null) {
       try {
         // call first and then get iteration
         subMsg();
         const id = setInterval(() => {
           subMsg();
         }, seconds);
-        setIntervalId(prev => id);
-        console.debug("add new interval id", id);
+        setIntervalId(id);
+        console.debug('add new interval id', id);
       } catch (error: any) {
-        console.debug("add failed, ", error.message);
+        console.debug('add failed, ', error.message);
       }
+    }else{
+      console.debug('use same interval id', intervalId);
     }
-  }
+  };
 
   useEffect(() => {
-    clearProcess(); 
+    clearProcess();
     startProcess(subIntervalSeconds * 1000);
 
     return () => {
-      console.debug("component destroyed..");
+      console.debug('component destroyed..');
       clearProcess();
     };
   }, [msgFilter]);
 }
-
