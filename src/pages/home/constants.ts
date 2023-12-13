98f1e928aa3031740da1a34e925dd49d9b6d4496
digi-monkey <digimonkey@protonmail.com>
2023-12-13 22:46:36 +0800

    fix: use different key to avoid break change for home tab/filter storage

--- src/pages/home/constants.ts
+++ src/pages/home/constants.ts
@@ -1,2 +1,2 @@
-export const SELECTED_TAB_KEY_STORAGE_KEY = 'home-selected-tab-key';
-export const SELECTED_FILTER_STORAGE_KEY = 'home-selected-filter';
+export const SELECTED_TAB_KEY_STORAGE_KEY = 'home-selected-tab-key-v1';
+export const SELECTED_FILTER_STORAGE_KEY = 'home-selected-filter-v1';
