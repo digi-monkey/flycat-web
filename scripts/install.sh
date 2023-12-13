376561132cc17d02a73f873d1da3afdad9007e33
ahonn <ahonn95@outlook.com>
2023-12-13 12:17:16 +0800

    chore: improve installation script for Rust and wasm-pack

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,23 @@
 echo "Pulling submodules..."
 git submodule update --init --recursive
 
-echo "Installing Rustup..."
-# Install Rustup (compiler)
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-# Adding binaries to path
-source "$HOME/.cargo/env"
+if ! [ -x "$(command -v rustup)" ]; then
+    echo "Installing Rustup..."
+    # Install Rustup (compiler)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    # Adding binaries to path
+    source "$HOME/.cargo/env"
+else
+    echo "Rustup is already installed."
+fi
 
-echo "Installing wasm-pack..."
-# Install wasm-pack
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+if ! [ -x "$(command -v wasm-pack)" ]; then
+    echo "Installing wasm-pack..."
+    # Install wasm-pack
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+else
+    echo "wasm-pack is already installed."
+fi
 
 echo "Building wasm..."
 wasm-pack build wasm/ --target web
