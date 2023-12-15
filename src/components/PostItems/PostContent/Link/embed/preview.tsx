65ee2c5c22e74696d827f25eb5afa66073ea8215
digi-monkey <digimonkey@protonmail.com>
2023-12-15 15:11:38 +0800

    chore: simplify LinkPreview style

--- src/components/PostItems/PostContent/Link/embed/preview.tsx
+++ src/components/PostItems/PostContent/Link/embed/preview.tsx
@@ -1,8 +1,9 @@
 import { UrlMetadata, getUrlMetadata } from 'core/ogp';
 import { getBaseUrl, maxStrings } from 'utils/common';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import styles from '../index.module.scss';
+import { URL } from 'url';
 
 export const URLPreview = ({ url }: { url: string }) => {
   const [data, setData] = useState<UrlMetadata>();
@@ -20,30 +21,55 @@ export const URLPreview = ({ url }: { url: string }) => {
     fetchData();
   }, []);
 
+  const describedUrl = useMemo(()=>{
+    let result: string | undefined;
+    if(data?.title && data?.description){
+      result = `${data.title}(${data.description.slice(0, 45)}..)`;
+      return result;
+    }
+    if(data?.title){
+      return `${data.title}(${new URL(url).hostname})`;
+    }
+    if(data?.description){
+      return  `${new URL(url).hostname}(${data.description.slice(0, 45)}..)`
+    }
+    return url;
+  }, [data])
+
   return (
-    <div
-      className={styles.preview}
-      onClick={(e) => {
-        e.stopPropagation();
-        window.open(url, '_blank');
-      }}
-    >
-      <div>
-        {<div className={styles.baseUrl}>{getBaseUrl(url)}</div>}
-        {data?.title && (
-          <div className={styles.siteTitle}>{maxStrings(data.title, 45)}</div>
-        )}
-        {data?.description && (
-          <div className={styles.siteDescription}>
-            {maxStrings(data.description, 100)}
+    <span>
+      <a
+        href=""
+        target="_blank"
+        className={styles.hoverLink}
+        onClick={e => e.stopPropagation()}
+      >
+        {describedUrl}
+      </a>
+      <div
+        className={styles.preview}
+        onClick={e => {
+          e.stopPropagation();
+          window.open(url, '_blank');
+        }}
+      >
+        <div>
+          {<div className={styles.baseUrl}>{getBaseUrl(url)}</div>}
+          {data?.title && (
+            <div className={styles.siteTitle}>{maxStrings(data.title, 45)}</div>
+          )}
+          {data?.description && (
+            <div className={styles.siteDescription}>
+              {maxStrings(data.description, 100)}
+            </div>
+          )}
+        </div>
+        {data?.image && (
+          <div className={styles.cover}>
+            <img src={data.image} alt={data.title} />
           </div>
         )}
       </div>
-      {data?.image && (
-        <div className={styles.cover}>
-          <img src={data.image} alt={data.title} />
-        </div>
-      )}
-    </div>
+    </span>
   );
 };
