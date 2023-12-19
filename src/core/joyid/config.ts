ccca8f6bbcf61cfa11e0a90fd6d1b989cf2b2737
ahonn <ahonn95@outlook.com>
2023-12-19 21:58:16 +0900

    fix: use joyid testnet when NODE_ENV is not production

--- src/core/joyid/config.ts
+++ src/core/joyid/config.ts
@@ -1,8 +1,8 @@
 export const joyIdConfig = {
-  // your app name
   name: 'flycat',
-  // your app logo,
   logo: 'https://flycat.club/logo512.png',
-  // optional, default to 'https://poc.joyid.dev'
-  joyidAppURL: 'https://app.joy.id',
+  joyidAppURL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://app.joy.id'
+      : 'https://testnet.joyid.dev',
 };
