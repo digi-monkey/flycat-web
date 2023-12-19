fb0fbf3f57311459989eab493f7459a70dc3bfdf
digi-monkey <105776364+digi-monkey@users.noreply.github.com>
2023-12-19 14:41:55 +0800

    chore: bump v0.2.7 (#394)

--- src/pages/home/updates.ts
+++ src/pages/home/updates.ts
@@ -1,8 +1,20 @@
 export const updates = [
   {
     content:
-      '[9.27] v0.2.5 release: add hashtags and nip05-name/dotbit-alias in user profile/article URL',
+      '[12.19] v0.2.7 release: refactor timeline with cache/improve performance/bug fixes',
     isNew: true,
+    url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.7',
+  },
+  {
+    content:
+      '[10.25] v0.2.6 release: add #meme filter/wasm script experiment and nip05 hosting',
+    isNew: false,
+    url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.6',
+  },
+  {
+    content:
+      '[9.27] v0.2.5 release: add hashtags and nip05-name/dotbit-alias in user profile/article URL',
+    isNew: false,
     url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.5',
   },
   {
