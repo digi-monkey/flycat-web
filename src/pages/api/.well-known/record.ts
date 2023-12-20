7069b4eb87becd8aab81c34e31e175b435d62554
digi-monkey <digimonkey@protonmail.com>
2023-12-20 13:51:26 +0800

    chore: assgin _ nip05 name to flycat officail account

--- src/pages/api/.well-known/record.ts
+++ src/pages/api/.well-known/record.ts
@@ -1,3 +1,4 @@
 export const records: Record<string, string> = {
-	monkey: '45c41f21e1cf715fa6d9ca20b8e002a574db7bb49e96ee89834c66dac5446b7a',
+  monkey: '45c41f21e1cf715fa6d9ca20b8e002a574db7bb49e96ee89834c66dac5446b7a',
+  _: 'f60140bb15e3f8e6a6032a557dcde4b900d52f315799b35052e625333a5cd904', // flycat official nostr account
 };
