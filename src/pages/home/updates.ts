a7531914c994b705c1031fcbb46c1abcc7aa930d
digi-monkey <105776364+digi-monkey@users.noreply.github.com>
2023-12-28 14:06:05 +0800

    chore: bump v0.2.8 (#433)

--- src/pages/home/updates.ts
+++ src/pages/home/updates.ts
@@ -1,24 +1,30 @@
 export const updates = [
   {
     content:
-      '[12.19] v0.2.7 release: refactor timeline with cache/improve performance/bug fixes',
+      '[12.19] v0.2.8: simplify filter options/refactor UI with TailwindCSS/bug fixes',
     isNew: true,
+    url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.8',
+  },
+  {
+    content:
+      '[12.19] v0.2.7: refactor timeline with cache/improve performance/bug fixes',
+    isNew: false,
     url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.7',
   },
   {
     content:
-      '[10.25] v0.2.6 release: add #meme filter/wasm script experiment and nip05 hosting',
+      '[10.25] v0.2.6: add #meme filter/wasm script experiment and nip05 hosting',
     isNew: false,
     url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.6',
   },
   {
     content:
-      '[9.27] v0.2.5 release: add hashtags and nip05-name/dotbit-alias in user profile/article URL',
+      '[9.27] v0.2.5: add hashtags and nip05-name/dotbit-alias in user profile/article URL',
     isNew: false,
     url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.5',
   },
   {
-    content: '[9.13]  v0.2.4 release with timeline refactoring',
+    content: '[9.13] v0.2.4: timeline refactoring',
     isNew: false,
     url: 'https://github.com/digi-monkey/flycat-web/releases/tag/v0.2.4',
   },
