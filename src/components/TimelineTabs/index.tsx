6bff0a7330c79a2387ab75f62b3373153aaf2c67
digi-monkey <digimonkey@protonmail.com>
2024-01-19 15:11:36 +0800

    chore: adjust filter opt tab and description

--- src/components/TimelineTabs/index.tsx
+++ src/components/TimelineTabs/index.tsx
@@ -9,6 +9,7 @@ export interface TimelineTabsProp {
   worker: CallWorker | undefined;
   defaultActiveKey?: string;
   onActiveKeyChanged?: (val: string) => any;
+  showDescription?: boolean;
 }
 
 export function TimelineTabs({
@@ -16,6 +17,7 @@ export function TimelineTabs({
   worker,
   defaultActiveKey,
   onActiveKeyChanged,
+  showDescription = true,
 }: TimelineTabsProp) {
   const [activeTabKey, setActiveTabKey] = useState<string>(
     defaultActiveKey || filterOptions[0].key,
@@ -39,7 +41,7 @@ export function TimelineTabs({
         <Tabs.List className="w-full flex justify-between overflow-scroll border-0 border-b border-solid border-b-gray-200">
           {filterOptions.map(val => (
             <Tabs.Trigger
-              className="font-poppins w-full cursor-pointer py-4 px-2 text-gray-600 font-medium whitespace-nowrap border-transparent bg-transparent border-0 data-[state=active]:text-green-700 data-[state=active]:border-b-2 data-[state=active]:border-green-500"
+              className="text-base font-medium font-poppins w-full cursor-pointer py-4 px-2 text-gray-600 whitespace-nowrap border-transparent bg-transparent border-0 data-[state=active]:text-green-700 data-[state=active]:border-b-2 data-[state=active]:border-green-500"
               key={val.key}
               value={val.key}
             >
@@ -51,9 +53,11 @@ export function TimelineTabs({
       <div className="px-2">
         {filterOptions.map(val => (
           <Tabs.Content key={val.key} value={val.key}>
-            <div className="mt-2 text-sm text-text-secondary border border-solid border-brand capitalize px-2 py-2 rounded-lg bg-primary-100">
-              {val.description}
-            </div>
+            {showDescription && (
+              <div className="mt-2 text-xs text-text-secondary border border-solid border-brand capitalize px-1 py-1 rounded-lg bg-primary-100">
+                {val.description}
+              </div>
+            )}
             <Timeline worker={worker} msgFilter={val} />
           </Tabs.Content>
         ))}
